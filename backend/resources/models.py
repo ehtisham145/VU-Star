2d3bed416a506dbfedf8378e2178cc63b0470595
ehtisham145 <ehtisham2406@gmail.com>
2026-07-21 12:41:32 +0500

    Completed Day 5 Work

--- backend/resources/models.py
+++ backend/resources/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.conf import settings
+from courses.models import Course, Semester
 
-# Create your models here.
+
+class Resource(models.Model):
+    class ResourceType(models.TextChoices):
+        HANDOUT = 'handout', 'Handout'
+        MIDTERM = 'midterm', 'Midterm Past Paper'
+        FINAL = 'final', 'Final Term Past Paper'
+        GDB = 'gdb', 'GDB'
+        QUIZ = 'quiz', 'Quiz'
+
+    title = models.CharField(max_length=200)
+    file = models.FileField(upload_to='resources/')
+    resource_type = models.CharField(
+        max_length=20,
+        choices=ResourceType.choices
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='resources'
+    )
+    semester = models.ForeignKey(
+        Semester,
+        on_delete=models.CASCADE,
+        related_name='resources'
+    )
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='uploaded_resources'
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    # This Allow only zero or postive numbers for count
+    downloads_count = models.PositiveIntegerField(default=0)
+    def __str__(self):
+        return self.title
